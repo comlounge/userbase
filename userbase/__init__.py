2df8e7f261902fb5929d3be7e2160bbeb35c66f6
Christian Scholz <cs@comlounge.net>
2012-02-15 18:24:39 +0100

    started a dummy login manager, needs to be moved though

---
+++ userbase/__init__.py
@@ -0,0 +1,9 @@
+class LoginManager(object):
+    """the login manager"""
+
+    def __init__(self, handler, config = {}):
+        """initialize login manager"""
+        self.handler = handler
+        self.config = config
+
+
