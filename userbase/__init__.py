f8598085fa9f05433a05635e8943463e12ce4279
Christian Scholz <cs@comlounge.net>
2012-02-28 16:32:53 +0100

    implemented new UserManager and LoginManager

--- userbase/__init__.py
+++ userbase/__init__.py
@@ -1,9 +1,93 @@
-class LoginManager(object):
+from core import *
+from paste.auth import auth_tkt
+
+
+class LoginAdapter(object):
     """the login manager"""
 
-    def __init__(self, handler, config = {}):
+    def __init__(self, handler, userdb):
         """initialize login manager"""
-        self.handler = handler
-        self.config = config
+        self.handler = handler # the handler we adapt to
+        self.userdb = userdb # the user database to use
+        self.config = userdb.config
+
+        # process the request and extract any login information
+        self.userid = None
+        self.user = None
+        req = self.request = self.handler.request
+        cookie_name = self.config.get("cookie_name", "u")
+        self.cookie_secret = self.config.get("cookie_name", "foo0bar")
+        if self.cookie_secret == "foo0bar":
+            print "please configure a cookie secret in the login manager settings!"
+
+        if req.cookies.has_key(cookie_name):
+            at = req.cookies[cookie_name]
+            try:
+                self.timestamp, self.userid, self.roles, self.token_attribs = auth_tkt.parse_ticket(
+                    self.cookie_secret, at, "127.0.0.1")
+                print "yes"
+            except auth_tkt.BadTicket, e:
+                print "no"
+                pass
+        else:
+            self.userid = None
+            self.user = None
+        if self.userid is not None:
+            self.user = self.userdb.get(self.userid) 
+            if self.user is None:
+                self.log.error("user in token not found: %s " %self.userid)
+                self.userid=None
+                self.user=None
+
+    def set_user(self, user):
+        """login a user"""
+        self.user = user
+        self.userid = unicode(user.d._id)
+        cookie_name = self.config.get("cookie_name", "u")
+        ticket = auth_tkt.AuthTicket(self.cookie_secret, self.userid, "127.0.0.1")
+        self.handler.response.set_cookie(cookie_name, ticket.cookie_value())
+        return ticket.cookie_value()
+
+    def logout(self):
+        """log a user out by deleting the cookie"""
+        cookie_name = self.config.get("cookie_name", "u")
+        self.handler.response.delete_cookie(cookie_name)
+         
+    @property
+    def logged_in(self):
+        """check if somebody is logged in"""
+        return self.user is not None
+   
+    def login(self, identifier, password):
+        """try to login the user
+
+        :param identifier: the configure identifier which defaults to the email address
+        :param password: the password to use
+
+        returns None if it hasn't worked, the User object otherwise
+
+        TODO: raise some error so we can pass the error cause and the client can do what it thinks is right
+
+        """
+
+        user = self.userdb[identifier]
+        if user is None:
+            return None
+        if not user.check_pw(password):
+            return None
+        self.user = user
+        return user
+
+    def redirect(self, url):
+        """do the redirect while setting the login cookie if it exists"""
+        if self.user is not None:
+            value = self.set_user(self.user)
+            cookie_name = self.config.get("cookie_name", "u")
+            raise self.handler.redirect(url, cookies = {cookie_name: value})
+        else:
+            raise self.handler.redirect(url)
+        
+            
+
 
 
--- userbase/core.py
+++ userbase/core.py
@@ -1,28 +1,31 @@
+from starflyer import AttributeMapper
 import colander
 import hashlib
 import uuid
 import datetime
-from base import Record, Collection, DateTime
-__all__ = ['User', 'Users']
+import mongoquery.onrm as onrm
+__all__ = ['User', 'UserManager', 'UserSchema']
 
 class Roles(colander.SequenceSchema):
     role = colander.SchemaNode(colander.String())
 
 class UserSchema(colander.MappingSchema):
+    """the most basic schema for a user. You can extend it to your liking though.
+    Note that we use the email address as main identifier but you can change this
+    yourself to any other field. You should make sure it's indexed though if you expect
+    a lot of users.
+    """
     email = colander.SchemaNode(colander.String(), validator = colander.Email())
-    username = colander.SchemaNode(colander.String(), missing="", default="")
     name = colander.SchemaNode(colander.String(), validator = colander.Length(6,200))
     password = colander.SchemaNode(colander.String(), validator = colander.Length(6))
-    bio = colander.SchemaNode(colander.String(), missing="")
     state = colander.SchemaNode(colander.String(), missing="initialized")
     validation_code = colander.SchemaNode(colander.String(), missing="")
-    validation_code_sent = colander.SchemaNode(DateTime(), missing="")
+    validation_code_sent = colander.SchemaNode(onrm.DateTime(), missing="")
     pw_code = colander.SchemaNode(colander.String(), missing="")
-    pw_code_sent = colander.SchemaNode(DateTime(), missing="")
-    last_logged_in = colander.SchemaNode(DateTime(), missing="")
-    roles = Roles(missing=[])
+    pw_code_sent = colander.SchemaNode(onrm.DateTime(), missing="")
+    last_logged_in = colander.SchemaNode(onrm.DateTime(), missing="")
 
-class User(Record):
+class User(onrm.Record):
     """A user record. 
 
     For validation we use the following workflow:
@@ -84,10 +87,29 @@ class User(Record):
         """save the object"""
         self.collection.put(self)
 
-class Users(Collection):
-    
+class UserManager(onrm.Collection):
+    """the user manager which handles users on the API level"""
+
     data_class = User
+    identifier = "email"
     use_objectids = False # does the mongodb collection use object ids?
+
+    def __init__(self, collection, config={}):
+        """initialize the user manager
+
+        :param collection: The MongoDB collection object to be used to store asset data
+        :param config: A dictionary containing configuration for the Media Database
+
+        """
+        super(UserManager, self).__init__(collection, config)
+
+    def on_put(self, obj, data):
+        """check for duplicate emails"""
+        if not obj.from_db:
+            email = obj.d.email
+            if self.find_by_email(email) is not None:
+                raise onrm.InvalidData({'email' : 'already exists'})
+        return data
 
     def find_by_email(self, email):
         """return a user by email or None"""
@@ -113,10 +135,12 @@ class Users(Collection):
             return None
         return res[0]
 
-
-
-
-
-
-
+    def __getitem__(self, ident):
+        """return the user defined by the identifier given"""
+        q = self.query
+        q.update(**{self.identifier : ident})
+        res = q()
+        if res.count == 0:
+            return None
+        return list(res)[0]
 
