979079f872dae7778612193f8df830dff96c06bd
Christian Scholz <cs@comlounge.net>
2012-09-11 18:07:39 +0200

    made registration form and validation work.

--- userbase/handlers/registration.py
+++ userbase/handlers/registration.py
@@ -1,21 +1,45 @@
 from starflyer import Handler, redirect
 from wtforms import Form, TextField, PasswordField, BooleanField, validators
+from wtforms import ValidationError
 from userbase import db
 
 __all__ = ['UsernameRegistrationForm', 'EMailRegistrationForm', 'RegistrationHandler']
 
-class UsernameRegistrationForm(Form):
+class BaseForm(Form):
+    """custom form class for passing in attributes so you can use them in validators. Here we
+    use it for the module and config itself
+    """
+    
+    def __init__(self, formdata=None, obj=None, prefix='', module=None, **kwargs):
+        super(BaseForm, self).__init__(formdata=formdata, obj=obj, prefix=prefix, **kwargs)
+        self.module = module                                                                                                                                                         
+
+class UsernameRegistrationForm(BaseForm):
+    username    = TextField('Username',     [validators.Length(max=200), validators.Required()])
     email       = TextField('E-Mail',       [validators.Length(max=200), validators.Email(), validators.Required()])
-    password    = PasswordField('Password', [validators.Length(max=135), validators.Required()])
+    password    = PasswordField('New Password', [validators.Required(), validators.EqualTo('password2', message='Passwords must match')])
     password2   = PasswordField('Password confirmation', [validators.Length(max=135), validators.Required()])
     fullname    = TextField('Full name',    [validators.Length(max=200), validators.Required()])
 
-class EMailRegistrationForm(Form):
-    username    = TextField('Username',     [validators.Length(max=200), validators.Required()])
+    def validate_email(form, field):
+        if form.module.users.find({'email' : field.data}).count() > 0:
+            raise ValidationError('this email is already taken')
+
+    def validate_username(form, field):
+        if form.module.users.find({'username' : field.data}).count() > 0:
+            raise ValidationError('this username is already taken')
+
+
+class EMailRegistrationForm(BaseForm):
     email       = TextField('E-Mail',       [validators.Length(max=200), validators.Email(), validators.Required()])
-    password    = PasswordField('Password', [validators.Length(max=135), validators.Required()])
+    password    = PasswordField('New Password', [validators.Required(), validators.EqualTo('password2', message='Passwords must match')])
     password2   = PasswordField('Password confirmation', [validators.Length(max=135), validators.Required()])
     fullname    = TextField('Full name',    [validators.Length(max=200), validators.Required()])
+
+    def validate_email(form, field):
+        if form.module.users.find({'email' : field.data}).count() > 0:
+            raise ValidationError('this email is already taken')
+
 
 class RegistrationHandler(Handler):
     """show the registration form and process it"""
@@ -29,7 +53,7 @@ class RegistrationHandler(Handler):
         form = cfg.registration_form
         obj_class = cfg.user_class
 
-        form = form(self.request.form)
+        form = form(self.request.form, module = self.module)
         if self.request.method == 'POST':
             if form.validate():
                 f = form.data
