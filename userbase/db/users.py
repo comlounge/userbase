8c2b46724fae20816809e078dcb323fd10020dc1
Christian Scholz <cs@comlounge.net>
2012-09-11 16:37:27 +0200

    the two class UserUsername and UserEMail are now one. This makes things easiert and in the email case it is sufficient to let the username field empty or so.

--- userbase/db/users.py
+++ userbase/db/users.py
@@ -15,13 +15,17 @@ class Password(CustomType):
 
     def to_bson(self, value):
         """convert type to a mongodb type"""
-        return hashlib.new("md5",value).hexdigest()
+        # for some reason this is called twice and thus the password is encoded twice. 
+        # so this is a workaround
+        if value.startswith("HASH:"):
+            return value
+        return "HASH:"+hashlib.new("md5",value).hexdigest()
 
     def to_python(self, value):
         """convert type to a python object"""
         return value
 
-__all__ = ['UserBase', 'UserEMail', 'UserUsername']
+__all__ = ['UserBase', 'User']
 
 class UserBase(object):
     """Base class for all users"""
@@ -30,7 +34,7 @@ class UserBase(object):
     def check_password(self, pw):
         """check password"""
         hash = hashlib.new("md5",pw).hexdigest()
-        return hash == self.password
+        return "HASH:"+hash == self.password
 
     def create_pw(self):
         """create a password"""
@@ -62,15 +66,16 @@ class UserBase(object):
 
     
 
-class UserEMail(Document, UserBase):
+class User(Document, UserBase):
     """a user identified by email address and password"""
 
-    name = "UserEMail" # should be the same name as the class
+    name = "User" # should be the same name as the class
     use_dot_notation = True
 
     structure = {
-        'fullname'                      : basestring,
+        'username'                      : basestring,
         'email'                         : basestring,
+        'fullname'                      : basestring,
         'password'                      : Password(),
         'date_creation'                 : datetime.datetime,
         'active'                        : bool,
@@ -104,50 +109,5 @@ class UserEMail(Document, UserBase):
     
     def get_id(self):
         """return the userid we want to use in sessions etc."""
-        return self.email
+        return self._id
     
-    
-class UserUsername(Document, UserBase):
-    """a user identified by a username and password"""
-
-    name = "UserUsername" # should be the same name as the class
-    use_dot_notation = True
-
-    structure = {
-        'email'                         : basestring,
-        'username'                      : basestring,
-        'password'                      : Password(),
-        'date_creation'                 : datetime.datetime,
-        'active'                        : bool,
-        'activation_time'               : datetime.datetime,
-        'last_login'                    : datetime.datetime,
-        'last_ip'                       : basestring,
-        'activation_code'               : basestring,
-        'password_code'                 : basestring,
-        'activation_code_expires'       : datetime.datetime,
-        'password_code_expires'         : datetime.datetime,
-        'fullname'                      : basestring,
-    }
-    
-    required_fields = ['email', 'username', 'password', 'date_creation']
-
-    validators = {
-        'email': email_validator,
-    }
-    
-    default_values = {
-        'date_creation'                 : datetime.datetime.utcnow,
-        "last_ip"                       : "",
-        "last_login"                    : None,
-        "activation_time"               : None,
-        "activation_code_expires"       : None,
-        "activation_code"               : None,
-        "password_code"                 : None,
-        "password_code_expires"         : None,
-        "active"                        : False,
-    }
-
-    def get_id(self):
-        """return the userid we want to use in sessions etc."""
-        return self.username
-
