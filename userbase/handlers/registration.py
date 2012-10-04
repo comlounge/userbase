5b453355a511f96e589020c61cea389cba4c58bf
Christian Scholz <cs@comlounge.net>
2012-10-04 11:34:21 +0200

    now showing different message and redirecting to different url if a registration ends with a pending double opt in

--- userbase/handlers/registration.py
+++ userbase/handlers/registration.py
@@ -21,15 +21,19 @@ class RegistrationHandler(Handler):
         if self.request.method == 'POST':
             if form.validate():
                 f = form.data
-                user = mod.register(f)
-                self.flash(cfg.messages.registration_success %user)
-                if cfg.login_after_registration:
+                user = mod.register(f, create_pw=False)
+                if cfg.login_after_registration and not cfg.use_double_opt_in:
                     user = mod.login(self, force=True, **f)
                     self.flash(cfg.messages.login_success %user)
                     url_for_params = cfg.urls.login_success
                     url = self.url_for(**url_for_params)
                     return redirect(url)
-                url_for_params = cfg.urls.registration_success
+                if cfg.use_double_opt_in:
+                    self.flash(cfg.messages.double_opt_in_pending %user)
+                    url_for_params = cfg.urls.double_opt_in_pending
+                else:
+                    self.flash(cfg.messages.registration_success %user)
+                    url_for_params = cfg.urls.registration_success
                 url = self.url_for(**url_for_params)
                 return redirect(url)
         return self.render(form = form)
