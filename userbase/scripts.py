075703a53b0c328bc1f3f92409a2e5abe9eefd54
Christian Scholz <cs@comlounge.net>
2016-01-05 17:27:41 +0100

    added command to set permissions for a user

--- userbase/scripts.py
+++ userbase/scripts.py
@@ -16,9 +16,13 @@ class UserManager(ScriptBase):
         self.add_parser.add_argument('password', help='password')
         self.add_parser.add_argument('--fullname', dest="fullname", default="", help='the full name')
 
-        self.add_parser = subparsers.add_parser('pw', help='adding users')
+        self.add_parser = subparsers.add_parser('pw', help='set password for a user')
         self.add_parser.add_argument('username', help='username')
         self.add_parser.add_argument('password', help='password')
+
+        self.add_parser = subparsers.add_parser('permissions', help='set permissions for a user')
+        self.add_parser.add_argument('username', help='username')
+        self.add_parser.add_argument('permissions', help='comma-separated list of permissions')
 
     def __call__(self):
         m = self.app.module_map['userbase']
@@ -35,11 +39,13 @@ class UserManager(ScriptBase):
             user.password = data['password']
             user.pw_code = None
             user.save()
+        elif data['cmd'] == "permissions":
+            user = m.get_user_by_username(data['username'])
+            permissions = data['permissions'].split(",")
+            permissions = [perm.strip() for perm in permissions]
+            user.permissions = permissions
+            user.save()
         
-
-
-
-
 def um():
     mgr = UserManager()
     mgr()
